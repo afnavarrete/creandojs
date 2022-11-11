3ccb7a29df01dd50b6cb23cbe1b003553ec20e28
antonela foresti <forestiantonela@gmail.com>
2022-11-11 16:13:38 -0300

    Primer clase de Javascript let, const, console, prompt y alert

---
+++ app.js
@@ -0,0 +1,28 @@
+//Declaracion de Variable "let"
+/*
+let nombre = "Jeremias"; // con comillas porque es un STRING
+let edad = 30; //sin comillas porque es un NUMBER
+let alumno = true; //sin comillas porque es un BOOLEANS solo admite true or false
+//en la web, click derecho inspeccionar Console
+console.log(nombre)
+console.log(edad)
+console.log(alumno)
+
+//Declaracion de Variable "const"
+const CURSO="JS";
+console.log(CURSO)
+
+//Concatenaciones van con "+"
+//declaracion let
+let nombre="Jimena"
+let apellido="Alcuaz"
+console.log(nombre+" "+apellido)
+
+
+//Prompt y Alert
+
+let nombre = prompt("Ingrese su nombre por favor")
+let apellido = prompt("Ingrese su apellido por favor")
+
+alert(nombre+" "+apellido)
+*/
