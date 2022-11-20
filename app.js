a4829b2a959edb1dd8c54b6ede3e996a62afcd8c
antonela foresti <forestiantonela@gmail.com>
2022-11-20 13:58:32 -0300

    agregue carrito.html y contacto.html + multimedia y css

--- app.js
+++ app.js
@@ -25,4 +25,22 @@ let nombre = prompt("Ingrese su nombre por favor")
 let apellido = prompt("Ingrese su apellido por favor")
 
 alert(nombre+" "+apellido)
+
+
+//Tarea en clase: crear algoritmo con prompt, compare entradas y segun muestre en consola o alert
+let contraseña = prompt("Ingrese su Contraseña Por Favor");
+if (contraseña == "AMOR" || contraseña == "amor") {
+    alert("La Contraseña Ingresada es Correcta");
+} else {
+    alert("La Contraseña Ingresada es Incorrecta");
+}
+//si la contraseña se escribe "AMOR" o "amor" la contraseña es correcta. Sino es incorrecta
+
+//Variables Boolean
+let añoNacimiento= prompt("Ingrese su Año de Nacimiento")
+if(añoNacimiento=="1990"){
+    alert("Su Año de Nacimiento es Correcto");
+} else {
+    alert("Su Año de Nacimiento es Incorrecto")
+}
 */
